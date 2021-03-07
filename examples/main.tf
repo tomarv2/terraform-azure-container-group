0c3b24817c9565524f9bd2e151479d7a5ae5895c
tomarv2 <varuntomar2019@gmail.com>
2021-03-07 09:05:36 -0800

    moving to new format

---
+++ examples/main.tf
@@ -0,0 +1,22 @@
+module "aci" {
+  source = "../"
+
+  email             = "demo@demo.com"
+  client_id         = var.client_id
+  client_secret     = var.client_secret
+  subscription_id   = var.subscription_id
+  tenant_id         = var.tenant_id
+  add_msi           = true
+  add_resourcegroup = true
+  rg_name           = "demo-rg"
+  docker_image      = "nginx"
+  container_port    = "80"
+  scopes            = "scopes"
+  env_variables = {
+    DEMO_ENV_VAR = "1"
+  }
+  # ---------------------------------------------
+  # Note: Do not change teamid and prjid once set.
+  teamid = var.teamid
+  prjid  = var.prjid
+}
--- main.tf
+++ main.tf
@@ -1,63 +1,64 @@
-locals{
-  rgname = var.add_resourcegroup != true ? var.rg_name : module.resourcegroup.resourcegroup_name[0]
+locals {
+  rgname        = var.add_resourcegroup != true ? var.rg_name : module.resourcegroup.resourcegroup_name[0]
   rgname_create = coalesce(var.rg_name, "${var.teamid}-${var.prjid}")
 
   msiname = var.add_msi == false ? var.msi_name : module.msi.msi_id[0]
 }
 
 module "resourcegroup" {
-  source                      = "git::git@github.com:tomarv2/terraform-azure-resource-group.git"
+  source = "git::git@github.com:tomarv2/terraform-azure-resource-group.git"
 
-  add_resourcegroup           = var.add_resourcegroup # FEATURE FLAG
+  add_resourcegroup = var.add_resourcegroup # FEATURE FLAG
 
-  rg_name                     = local.rgname_create
-  email                       = var.email
-  teamid                      = var.teamid
-  prjid                       = var.prjid
-  client_id                   = var.client_id
-  subscription_id             = var.subscription_id
-  tenant_id                   = var.tenant_id
-  client_secret               = var.client_secret
-  rg_location                 = var.rg_location
+  rg_name         = local.rgname_create
+  email           = var.email
+  teamid          = var.teamid
+  prjid           = var.prjid
+  client_id       = var.client_id
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_secret   = var.client_secret
+  rg_location     = var.rg_location
 }
 
 module "msi" {
-  source                      = "git::git@github.com:tomarv2/terraform-azure-msi.git"
+  source = "git::git@github.com:tomarv2/terraform-azure-msi.git"
 
-  add_msi                     = var.add_msi # FEATURE FLAG
+  add_msi = var.add_msi # FEATURE FLAG
 
-  email                       = var.email
-  teamid                      = var.teamid
-  prjid                       = var.prjid
-  client_id                   = var.client_id
-  subscription_id             = var.subscription_id
-  tenant_id                   = var.tenant_id
-  client_secret               = var.client_secret
-  rg_name                     = local.rgname
-  msi_location                = var.rg_location
+  email           = var.email
+  teamid          = var.teamid
+  prjid           = var.prjid
+  client_id       = var.client_id
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_secret   = var.client_secret
+  rg_name         = local.rgname
+  msi_location    = var.rg_location
 
-  msi_depends_on              = local.rgname
+  msi_depends_on = local.rgname
 }
 
 module "role_assignment" {
-  source = "../.."
+  source = "git::git@github.com:tomarv2/terraform-azure-role-assignment.git"
 
-  email                                   = var.email
-  teamid                                  = var.teamid
-  prjid                                   = var.prjid
-  client_id                               = var.client_id
-  client_secret                           = var.client_secret
-  subscription_id                         = var.subscription_id
-  tenant_id                               = var.tenant_id
-  principal_id                            = module.msi.msi_principal_id
-  scopes                                  = var.scopes
+
+  email           = var.email
+  teamid          = var.teamid
+  prjid           = var.prjid
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  principal_id    = module.msi.msi_principal_id
+  scopes          = var.scopes
 }
 
 locals {
   shared_tags = map(
-      "Name", "${var.teamid}-${var.prjid}",
-      "Owner", var.email,
-      "Team", var.teamid,
-      "Project", var.prjid
+    "name", "${var.teamid}-${var.prjid}",
+    "owner", var.email,
+    "team", var.teamid,
+    "project", var.prjid
   )
 }
